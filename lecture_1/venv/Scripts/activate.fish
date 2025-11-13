d8817cc51f3723a7ed23c2fc507e73c64b9fa032
Kirill <kirilldubejko6@gmail.com>
2025-11-13 23:25:05 +0300

    second try

--- lecture_1/venv/Scripts/activate.fish
+++ lecture_1/venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\innowise_laboratory\venv'
+set -gx VIRTUAL_ENV 'c:\innowise_laboratory\lecture_1\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
